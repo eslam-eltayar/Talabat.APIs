e9d5ab11d8972e3faa17f2796199f7e9b8bcdaaa
Eslam Eltayar <tayaruwk@gmail.com>
2024-04-12 00:21:35 +0200

    Get Products Endpoint

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -13,5 +13,15 @@ namespace Talabat.APIs.Controllers
         {
 			_productsRepo = productsRepo;
 		}
+
+
+		// /api/Products
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+		{
+			var products = await _productsRepo.GetAllAsync();
+
+			return Ok(products);
+		}
     }
 }
--- Talabat.Infrastructure/GenericRepository.cs
+++ Talabat.Infrastructure/GenericRepository.cs
@@ -20,7 +20,11 @@ namespace Talabat.Infrastructure
 		}
         public async Task<IEnumerable<T>> GetAllAsync()
 		{
-			return await _dbContext.Set<T>().ToListAsync();
+			if (typeof(T) == typeof(Product))
+				return (IEnumerable<T>) await  _dbContext.Set<Product>().Include(P => P.Brand).Include(P => P.Category).ToListAsync();
+
+
+			return await _dbContext.Set<T>().AsNoTracking().ToListAsync();
 		}
 
 		public async Task<T?> GetAsync(int id)
