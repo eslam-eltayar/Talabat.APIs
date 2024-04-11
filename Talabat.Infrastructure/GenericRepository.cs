7fad199e889dd1aae75a1dbc15178c8269b2bb79
Eslam Eltayar <tayaruwk@gmail.com>
2024-04-11 23:20:03 +0200

    Generic Repository

---
+++ Talabat.Infrastructure/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Infrastructure.Data;
+
+namespace Talabat.Infrastructure
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public GenericRepository(ApplicationDbContext dbContext)
+        {
+			_dbContext = dbContext;
+		}
+        public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _dbContext.Set<T>().ToListAsync();
+		}
+
+		public async Task<T?> GetAsync(int id)
+		{
+			return await _dbContext.Set<T>().FindAsync(id);
+		}
+	}
+}
