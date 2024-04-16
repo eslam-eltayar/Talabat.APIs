ba94a41db0c6c9094d14f3490cd9c14054b9c5ec
Eslam Eltayar <tayaruwk@gmail.com>
2024-04-16 03:04:57 +0200

    Part 04 Employee Example - Draft

---
+++ Talabat.APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Core.Specifications.Employee_Specs;
+
+namespace Talabat.APIs.Controllers
+{
+	
+	public class EmployeeController : BaseApiController
+	{
+		private readonly IGenericRepository<Employee> _employeeRepo;
+
+		public EmployeeController(IGenericRepository<Employee> employeeRepo)
+        {
+			_employeeRepo = employeeRepo;
+		}
+
+
+		[HttpGet] // GET : /api/Employees 
+		public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+		{
+			var spec = new EmployeeWithDepartmentSpecification();
+
+			var employee = await _employeeRepo.GetAllWithSpecAsync(spec);
+
+			return Ok(employee);
+		}
+
+		[HttpGet("{id}")] // GET : /api/Employees/1
+		public async Task<ActionResult<Employee>> GetEmployee(int id)
+		{
+			var spec = new EmployeeWithDepartmentSpecification(id);
+
+			var employee = await _employeeRepo.GetWithSpecAsync(spec);
+
+			if (employee is null)
+				return NotFound();
+
+			return Ok(employee);
+		}
+
+	}
+}
