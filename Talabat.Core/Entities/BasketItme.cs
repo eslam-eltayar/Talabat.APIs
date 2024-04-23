b5f5a610be6c578829528f6d87a2c636cdadd945
Eslam Eltayar <tayaruwk@gmail.com>
2024-04-23 02:07:32 +0200

    Basket Module Entities

---
+++ Talabat.Core/Entities/BasketItme.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Talabat.Core.Entities
+{
+	public class BasketItme
+	{
+		public int Id { get; set; }
+		public string ProductName { get; set; } = null!;
+		public string PictureUrl { get; set; } = null!;
+		public decimal Price { get; set; }
+		public string Category { get; set; } = null!;
+		public string Brand { get; set; } = null!;
+		public int Quantity { get; set; }
+
+
+	}
+}
---
+++ Talabat.Core/Entities/CustomerBasket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	public class CustomerBasket
+	{
+        public string Id { get; set; }
+		public List<BasketItme> Itmes { get; set; }
+	}
+}
