c05bb20f780d3599a9d4563d761d2cc271af7f28
Eslam Eltayar <tayaruwk@gmail.com>
2024-04-30 04:56:10 +0300

    Create Login End Point

--- Talabat.APIs/Controllers/AccountController.cs
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -10,10 +10,13 @@ namespace Talabat.APIs.Controllers
     public class AccountController :BaseApiController
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         // 1. Register
 
@@ -43,5 +46,25 @@ namespace Talabat.APIs.Controllers
         }
 
         // 2. Login
+
+        [HttpPost("Login")] // POST: /api/Login
+
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user is null) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+
+            if(!result.Succeeded) return Unauthorized(new ApiResponse(401,"Invalid Login"));
+
+            return Ok(new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "ThisWillBeTokenSooon"
+            });
+        }
+
     }
 }
