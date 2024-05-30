bafb7d1ca2fd63e1d9b4925be660663f69dc09c9
Eslam Eltayar <tayaruwk@gmail.com>
2024-05-30 20:10:51 +0300

    Impelement Unit Of Work

---
+++ Talabat.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core;
+using Talabat.Core.Entities;
+using Talabat.Core.Entities.Order_Aggregate;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Infrastructure.Data;
+
+namespace Talabat.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _dbContext;
+
+        // private Dictionary<string, GenericRepository<BaseEntity>> _repositories;
+
+        private Hashtable _repositories;
+        public UnitOfWork(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+            _repositories = new Hashtable();
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            var key = typeof(TEntity).Name; // ex: Order
+
+            if (!_repositories.ContainsKey(key))
+            {
+                var repository = new GenericRepository<TEntity>(_dbContext);
+
+                _repositories.Add(key, repository);
+            }
+
+            return _repositories[key] as IGenericRepository<TEntity>;
+        }
+
+        public async Task<int> CompleteAsync()
+        => await _dbContext.SaveChangesAsync();
+
+        public async ValueTask DisposeAsync()
+        => await _dbContext.DisposeAsync();
+
+
+    }
+}
