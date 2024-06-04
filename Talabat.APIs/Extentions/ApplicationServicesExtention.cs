781bb1e34b46ef6178171290f08638665af2944f
Eslam Eltayar <tayaruwk@gmail.com>
2024-06-04 05:56:25 +0300

    Refactor ProductController & Create Product Service

--- Talabat.APIs/Extentions/ApplicationServicesExtention.cs
+++ Talabat.APIs/Extentions/ApplicationServicesExtention.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Talabat.APIs.Errors;
 using Talabat.APIs.Helpers;
 using Talabat.Application.OrderService;
+using Talabat.Application.ProductService;
 using Talabat.Core;
 using Talabat.Core.Repositories.Contract;
 using Talabat.Core.Services.Contract;
@@ -10,44 +11,46 @@ using Talabat.Infrastructure;
 
 namespace Talabat.APIs.Extentions
 {
-	public static class ApplicationServicesExtention
-	{
-		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-		{
-			services.AddScoped(typeof(IOrderService), typeof(OrderService));
+    public static class ApplicationServicesExtention
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IProductService), typeof(ProductService));
 
-			services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
+            services.AddScoped(typeof(IOrderService), typeof(OrderService));
 
-			// Allow DI for IBasketRepository
-			services.AddScoped(typeof(IBasketRepository), typeof(BasketRepository));
+            services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
 
-			services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            // Allow DI for IBasketRepository
+            services.AddScoped(typeof(IBasketRepository), typeof(BasketRepository));
 
-			//services.AddAutoMapper(M => M.AddProfile(new MappingProfiles()));
-			services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
-
-			/// Validation Error Handling
-			services.Configure<ApiBehaviorOptions>(options =>
-			{
-				options.InvalidModelStateResponseFactory = (actionContext) =>
-				{
-					var errors = actionContext.ModelState.Where(P => P.Value.Errors.Count > 0)
-														  .SelectMany(P => P.Value.Errors)
-														  .Select(E => E.ErrorMessage)
-														  .ToList();
+            //services.AddAutoMapper(M => M.AddProfile(new MappingProfiles()));
+            services.AddAutoMapper(typeof(MappingProfiles));
 
 
-					var response = new ApiValidationErrorResponse()
-					{
-						Errors = errors
-					};
+            /// Validation Error Handling
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = (actionContext) =>
+                {
+                    var errors = actionContext.ModelState.Where(P => P.Value.Errors.Count > 0)
+                                                          .SelectMany(P => P.Value.Errors)
+                                                          .Select(E => E.ErrorMessage)
+                                                          .ToList();
 
-					return new BadRequestObjectResult(response);
-				};
-			});
 
-			return services;
-		}
-	}
+                    var response = new ApiValidationErrorResponse()
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(response);
+                };
+            });
+
+            return services;
+        }
+    }
 }
