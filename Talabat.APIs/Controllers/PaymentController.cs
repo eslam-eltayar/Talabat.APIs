7eb80a8a9af664f4f50ecbaff88298b8a1a8e9fd
Eslam Eltayar <tayaruwk@gmail.com>
2024-06-22 02:02:28 +0300

    Refactor CustomerBasket DTO

--- Talabat.APIs/Controllers/PaymentController.cs
+++ Talabat.APIs/Controllers/PaymentController.cs
@@ -19,7 +19,7 @@ namespace Talabat.APIs.Controllers
 
         [ProducesResponseType(typeof(CustomerBasket), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status400BadRequest)]
-        [HttpGet("{basketid}")] // GET : /api/Payment/{basketid}
+        [HttpGet("{basketId}")] // GET : /api/Payment/{basketid}
         public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
         {
             var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
--- Talabat.APIs/DTOs/CustomerBasketDto.cs
+++ Talabat.APIs/DTOs/CustomerBasketDto.cs
@@ -2,10 +2,16 @@
 
 namespace Talabat.APIs.DTOs
 {
-	public class CustomerBasketDto
-	{
+    public class CustomerBasketDto
+    {
         [Required]
         public string Id { get; set; }
+        [Required]
         public List<BasketItemDto> Items { get; set; }
+
+        public string? PaymentIntentId { get; set; }
+        public string? ClientSecret { get; set; }
+        public int? DeliveryMethodId { get; set; }
+        public decimal ShippingPrice { get; set; }
     }
 }
